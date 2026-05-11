d6e0930ff71dda9096d5ade57d504a3196e972e6
Sekar Tanjung Maulidia <jeonskr9@gmail.com>
2026-05-11 19:52:41 +0700

    UI: Use Tailwind CDN permanently to fix deployment styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,20 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;800&display=swap" rel="stylesheet">
     
-    <link rel="stylesheet" href="{{ asset('build/assets/app-MRi5hf-N.css') }}">
-    <script type="module" src="{{ asset('build/assets/app-YtA_lim_.js') }}"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            darkMode: 'class',
+            theme: {
+                extend: {
+                    fontFamily: {
+                        sans: ['Outfit', 'sans-serif'],
+                    }
+                }
+            }
+        }
+    </script>
+    <?php array_map('unlink', glob(storage_path('framework/views/*.php'))); ?>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
     <style>
