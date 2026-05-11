dccf14fa3fa8faf7a663829a854b0077f7b24a1f
Sekar Tanjung Maulidia <jeonskr9@gmail.com>
2026-05-11 19:21:38 +0700

    Security: Force HTTPS URLs when in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         // Global Database Connection Check
         try {
             DB::connection()->getPdo();
