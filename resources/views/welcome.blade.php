91207f2ce4fd176447c676a52e0aab6dfd07a15e
Sekar Tanjung Maulidia <jeonskr9@gmail.com>
2026-05-12 10:58:08 +0700

    Fix: Update hero image extension to .jpeg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -415,7 +415,7 @@
             <!-- Image with Vignette/Blur Effect -->
             <div class="mb-10 relative inline-block group">
                 <div class="absolute inset-0 bg-indigo-500/10 blur-[60px] rounded-full scale-90 group-hover:scale-110 transition duration-1000"></div>
-                <img src="{{ asset('img/rw04_sign.jpg') }}?v=1.3" 
+                <img src="{{ asset('img/rw04_sign.jpeg') }}?v=1.3" 
                      class="relative z-10 w-full max-w-[450px] mx-auto rounded-[3rem] shadow-2xl vignette-image"
                      alt="RW 04 Kalitanjung Timur">
             </div>
